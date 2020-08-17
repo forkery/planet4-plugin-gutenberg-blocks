94b614e30eb6244187e33fc2927b30e1c2196a3f
Maud Leray <maudleray035@gmail.com>
2020-08-17 11:01:00 +0200

    PLANET-5325 Add tablet styles

--- public/js/post_action.js
+++ public/js/post_action.js
@@ -31,16 +31,20 @@ jQuery(function ($) {
   function scroll_action_card_scroll() {
     const postStart = $('.post-content').position().top;
     const postHeight = $('.post-content').outerHeight();
-    if ($(window).scrollTop() > postStart && $(window).scrollTop() < (postHeight - postStart)) {
-      $boxoutScroll.css('display', 'flex');
-      // If mobile/tablet the user can close the boxout by clicking on "not now"
-      if ($(window).width() < 992) {
-        $('.not-now').on('click', () => {
-          $boxoutScroll.addClass('hidden');
-        });
+    // There should be no scrolling on tablet
+    const shouldScroll = $(window).width() < 768 || $(window).width() > 992;
+    if (shouldScroll) {
+      if ($(window).scrollTop() > postStart && $(window).scrollTop() < (postHeight - postStart)) {
+        $boxoutScroll.css('display', 'flex');
+        // If mobile/tablet the user can close the boxout by clicking on "not now"
+        if ($(window).width() < 992) {
+          $('.not-now').on('click', () => {
+            $boxoutScroll.addClass('hidden');
+          });
+        }
+      } else if ($(window).width() > 992 || $(window).scrollTop() > (postHeight - postStart)) {
+        $boxoutScroll.css('display', 'none');
       }
-    } else if ($(window).width() > 992 || $(window).scrollTop() > (postHeight - postStart)) {
-      $boxoutScroll.css('display', 'none');
     }
   }
 
